54adc1bbb080b181ba851e65d04d6103f83871bc
zyuan4 <zyuan4@student.unimelb.edu.au>
2019-04-11 20:51:48 +1000

    add some products schema -price picture userscomment and rating

--- models/products.js
+++ models/products.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const ProductSchema = mongoose.Schema(
+ProductSchema = mongoose.Schema(
     {
         name: String,
+        price: {data: Buffer, contentType: String},
         description: String,
-        category: String
+        category: String,
+        rating: {type: Number, min: 0, max: 10},
+        usersComments: String
     }
+
+
 );
 
 mongoose.model('Products', ProductSchema);
