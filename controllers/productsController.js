b82bfac9eb586aecbf30452043a451d6800ece8b
Mingyu Su <msu2@student.unimelb.edu.au>
2019-04-12 00:33:10 +1000

    Updated feature: - A product can only be added to the database if the associated category exists in the database.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Products = mongoose.model('Products');
+const Categories = mongoose.model('Categories');
 
 // Load all the products.
 let allProducts = function(req, res) {
@@ -31,13 +32,22 @@ let addProducts = function(req, res) {
         description: req.body.description,
         category: req.body.category,
     });
-    newProduct.save(function(err,newProduct){
-        if(!err){
-            res.send(newProduct);
-        }else{
-            res.sendStatus(400);
+
+    // Check whether the category of the new product exists in our database.
+    Categories.findOne({name: newProduct.category}, function(err, category) {
+        if (category) {
+            newProduct.save(function(err, newProduct){
+                if(!err){
+                    res.send(newProduct);
+                }else{
+                    res.sendStatus(400);
+                }
+            });
+        } else {
+            res.send("The category is not defined.");
         }
     });
+    
 };
 
 module.exports.allProducts = allProducts;
