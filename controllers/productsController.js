ebc87e73b555f109fb0648941e2f3f61b66ebb08
zyuan4 <zyuan4@student.unimelb.edu.au>
2019-04-17 23:33:47 +1000

    - Restructure the upload image function.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose');
 const Products = mongoose.model('Products');
 const Categories = mongoose.model('Categories');
-require('express');
-
-// const multer= require('multer');
-//
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb) {
-//         cb(null, './uploads/');
-//     },
-//     filename: function(req, file, cb) {
-//         cb(null, new Date().toISOString() + file.originalname);
-//     }
-// });
-//
-// const upload = multer({storage: storage}).single('image');
 
 // Load all the products.
 let allProducts = function(req, res) {
@@ -39,42 +25,36 @@ let findProductByCategory = function(req, res) {
     });
 };
 
-// Add a new product.
-// let addProducts = function(req, res) {
-//     upload(req,res, function (err) {
-//         if(!err) {
-//             console.log(req.file);
-//         } else{
-//             res.sendStatus(400);
-//         }
-//     });
-//     //console.log(req.file);
-//     let newProduct = new Products({
-//         name: req.body.name,
-//         price: req.body.price,
-//         description: req.body.description,
-//         category: req.body.category,
-//         rating: req.body.rating,
-//         comments: req.body.rating
-//     });
-//
-//     // Check whether the category of the new product exists in our database.
-//     Categories.findOne({name: newProduct.category}, function(err, category) {
-//         if (category) {
-//             newProduct.save(function(err, newProduct){
-//                 if(!err){
-//                     res.send(newProduct);
-//                 }else{
-//                     res.sendStatus(400);
-//                 }
-//             });
-//         } else {
-//             res.send("The category is not defined.");
-//         }
-//     });
-//
-// };
+//Add a new product.
+let addProducts = function(req, res) {
+    console.log(req.file)
+    let newProduct = new Products({
+        name: req.body.name,
+        image: req.file.path,
+        price: req.body.price,
+        description: req.body.description,
+        category: req.body.category,
+        rating: req.body.rating,
+        comments: req.body.rating
+    });
+
+    // Check whether the category of the new product exists in our database.
+    Categories.findOne({name: newProduct.category}, function(err, category) {
+        if (category) {
+            newProduct.save(function(err, newProduct){
+                if(!err){
+                    res.send(newProduct);
+                }else{
+                    res.sendStatus(400);
+                }
+            });
+        } else {
+            res.send("The category is not defined.");
+        }
+    });
+
+};
 
 module.exports.allProducts = allProducts;
 module.exports.findProductByCategory = findProductByCategory;
-//module.exports.addProducts = addProducts;
+module.exports.addProducts = addProducts;
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose');
-const Products = mongoose.model('Products');
-const Categories = mongoose.model('Categories');
-
 const express = require('express');
 const router = express.Router();
-const multer= require('multer');
 
+// Used to upload image.
+const multer= require('multer');
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, './uploads/');
@@ -14,10 +11,7 @@ const storage = multer.diskStorage({
         cb(null, new Date().toISOString() + file.originalname);
     }
 })
-
-const upload = multer({storage: storage});//.single('image');
-
-
+const upload = multer({storage: storage});
 
 const controllers = require('../controllers/controllers.js');
 const productsController = require('../controllers/productsController.js');
@@ -38,34 +32,7 @@ router.get('/products', productsController.allProducts);
 router.get('/products/:category', productsController.findProductByCategory);
 
 // Add products.
-router.post('/products/addProduct', upload.single('image'),function(req,res){//productsController.addProducts);
-    console.log(req.file);
-    let newProduct = new Products({
-        name: req.body.name,
-        image: req.file.path,
-        price: req.body.price,
-        description: req.body.description,
-        category: req.body.category,
-        rating: req.body.rating,
-        comments: req.body.rating
-    });
-
-    //Check whether the category of the new product exists in our database.
-    Categories.findOne({name: newProduct.category}, function(err, category) {
-        if (category) {
-            newProduct.save(function(err, newProduct){
-                if(!err){
-                    res.send(newProduct);
-                }else{
-                    res.sendStatus(400);
-                }
-            });
-        } else {
-            res.send("The category is not defined.");
-        }
-    });
-});
-
+router.post('/products/addProduct', upload.single('image'),productsController.addProducts);
 
 // Display all the categories
 router.get('/categories', categoriesController.allCategories);
