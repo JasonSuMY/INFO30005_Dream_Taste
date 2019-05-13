aaa184bc99e86f26f29eebd9fda6adf7f507ebd7
Mingyu Su <msu2@student.unimelb.edu.au>
2019-05-13 16:26:17 +1000

    Updates:
    - Add a valiation at the back-end.

--- models/products.js
+++ models/products.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 ProductSchema = mongoose.Schema(
     {
-        name: {type: String, required: true},
-        image: {type: String, required: true},
+        name: {type: String, required: [true, "Product name must be input"]},
+        image: {type: String, required: [true, "An image must be added"]},
         price: String,
         description: String,
-        category: String,
+        category: {type: String, required: [true, "Category must be selected"]},
         rating: {type: Number, min: 0, max: 10},
         comments: String
     }
